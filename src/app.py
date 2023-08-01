1f8d07f4f474675b3eb85a01c040e71f658ec399
Cameron Gallichan <camerongallichan@gmail.com>
2023-08-01 02:18:49 +0100

    fixed db init & request handling

--- src/app.py
+++ src/app.py
@@ -10,6 +10,8 @@ from datetime import datetime, timedelta
 from functools import wraps
 from dotenv import load_dotenv
 
+debug = True
+
 if (load_dotenv() != True):
     exit('Failed to initialize environment')
 
@@ -20,15 +22,19 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('API_KEY')
 
 # db configuration
-db_name = os.getenv('MYSQL_DATABASE')
-db_user = os.getenv('MYSQL_USER')
-db_pass = os.getenv('MYSQL_PASSWORD')
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{db_user}:{db_pass}@localhost:3306/{db_name}'
+db_name = os.getenv('DB_NAME')
+db_user = os.getenv('DB_USER')
+db_pass = os.getenv('DB_PASS')
+
+if (debug == True):
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///webinator.sqlite3'
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{db_user}:{db_pass}@localhost/{db_name}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 # create SQLALCHEMY object
 db = SQLAlchemy(app)
-  
+
 # Database ORMs
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -36,7 +42,9 @@ class User(db.Model):
     name = db.Column(db.String(100))
     email = db.Column(db.String(70), unique = True)
     password = db.Column(db.String(80))
-  
+
+app.app_context().push()
+
 # decorator for verifying the JWT
 def token_required(f):
     @wraps(f)
@@ -47,7 +55,7 @@ def token_required(f):
             token = request.headers['x-access-token']
         # return 401 if token is not passed
         if not token:
-            return jsonify({'message' : 'Token is missing !!'}), 401
+            return jsonify({'message': 'Token is missing'}), 401
   
         try:
             # decoding the payload to fetch the stored details
@@ -57,7 +65,7 @@ def token_required(f):
                 .first()
         except:
             return jsonify({
-                'message' : 'Token is invalid !!'
+                'message' : 'Token is invalid'
             }), 401
         # returns the current logged in users context to the routes
         return  f(current_user, *args, **kwargs)
@@ -68,7 +76,7 @@ def token_required(f):
 # this route sends back list of users
 @app.route('/user', methods =['GET'])
 @token_required
-def get_all_users():
+def get_all_users(request_user):
     # querying the database
     # for all the entries in it
     users = User.query.all()
@@ -90,18 +98,18 @@ def get_all_users():
 @app.route('/login', methods =['POST'])
 def login():
     # creates dictionary of form data
-    auth = request.form
+    auth = request.get_json()
   
-    if not auth or not auth.get('email') or not auth.get('password'):
+    if not auth['email'] or not auth['password']:
         # returns 401 if any email or / and password is missing
         return make_response(
             'Could not verify',
             401,
-            {'WWW-Authenticate' : 'Basic realm ="Login required !!"'}
+            {'WWW-Authenticate': 'Basic realm ="Login required"'}
         )
   
     user = User.query\
-        .filter_by(email = auth.get('email'))\
+        .filter_by(email = auth['email'])\
         .first()
   
     if not user:
@@ -109,33 +117,33 @@ def login():
         return make_response(
             'Could not verify',
             401,
-            {'WWW-Authenticate' : 'Basic realm ="User does not exist !!"'}
+            {'WWW-Authenticate': 'Basic realm ="User does not exist"'}
         )
   
-    if check_password_hash(user.password, auth.get('password')):
+    if check_password_hash(user.password, auth['password']):
         # generates the JWT Token
         token = jwt.encode({
             'public_id': user.public_id,
             'exp' : datetime.utcnow() + timedelta(minutes = 30)
         }, app.config['SECRET_KEY'])
   
-        return make_response(jsonify({'token' : token.decode('UTF-8')}), 201)
+        return make_response(jsonify({'token': token.decode('UTF-8')}), 201)
     # returns 403 if password is wrong
     return make_response(
         'Could not verify',
         403,
-        {'WWW-Authenticate' : 'Basic realm ="Wrong Password !!"'}
+        {'WWW-Authenticate': 'Basic realm ="Wrong Password"'}
     )
-  
+
 # signup route
 @app.route('/signup', methods =['POST'])
 def signup():
     # creates a dictionary of the form data
-    data = request.form
+    data = request.get_json()
   
     # gets name, email and password
-    name, email = data.get('name'), data.get('email')
-    password = data.get('password')
+    name, email = data['name'], data['email']
+    password = data['password']
   
     # checking for existing user
     user = User.query\
@@ -156,8 +164,8 @@ def signup():
         return make_response('Successfully registered.', 201)
     else:
         # returns 202 if user already exists
-        return make_response('User already exists. Please Log in.', 202)
-  
+        return make_response('This email is already registered to an account. Please Log in.', 202)
+
 if __name__ == "__main__":
     # setting debug to True enables hot reload
     # and also provides a debugger shell
